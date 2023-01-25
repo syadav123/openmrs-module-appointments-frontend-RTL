000704aaa07d17707cca98f0414a8a4ca4f8ca8a
shilpa-iplit <sparab@iplit.in>
2023-01-25 20:54:29 +0530

    i18n locale ar changes

--- i18n/appointments/index.js
+++
@@ -1,7 +0,0 @@
-import en from './locale_en.json';
-import es from './locale_es.json';
-import fr from './locale_fr.json';
-import ptBr from './locale_pt_BR.json';
-import ar from './locale_ar.json';
-
-export default{en, es, fr, ptBr, ar};
