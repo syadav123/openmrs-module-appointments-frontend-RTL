04bb33e6bfdf5bb00e96431eafe1068d7a9b02dc
shilpa-iplit <sparab@iplit.in>
2023-03-04 13:12:50 +0530

    Fixing bugs

--- src/services/appointmentsServiceService.js
+++ src/services/appointmentsServiceService.js
@@ -5,7 +5,9 @@ angular.module('bahmni.appointments')
         function ($http) {
             this.save = function (service) {
                 service.name = encodeURIComponent(service.name);
-                service.description = encodeURIComponent(service.description || '');
+                if (service.description && service.description.length > 0) {
+                    service.description = encodeURIComponent(service.description);
+                }
                 return $http.post(Bahmni.Appointments.Constants.createServiceUrl, service, {
                     withCredentials: true,
                     headers: {"Accept": "application/json", "Content-Type": "application/json"}
